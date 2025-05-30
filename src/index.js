99493c518763622847e1856a12b5ef5a8a9b06e3
Simón Echeverri <simon.echeverri2003@gmail.com>
2025-05-30 17:06:07 -0500

    Actualizo API_URL a nuevo endpoint de Apps Script

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="849478111086-gktqn4c3o5c1h1ji1svgu68avor7j9eh.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
